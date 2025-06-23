6b5536e0224a52e97fcbbb2e65bb1f0c127b0d55
2ashtonm <165822893+2ashtonm@users.noreply.github.com>
2025-06-23 10:11:44 -0400

    Add functionality to use FFL api (#16)
    Add UI and state for FFL search params
    
    Pass in and use seperate SS locations object
    
    Add ffl serach function and seperate SS locations into seperate object
    
    Add styles for FFL serach params UI
    
    Make param fields consistent with other inputs
    
    Remove comments
    
    Add dynamic FFL search
    
    Fix Shoot Straight location switch

--- packages/core/src/app/fflShipping/FFLSelectOption.tsx
+++ packages/core/src/app/fflShipping/FFLSelectOption.tsx
@@ -9,7 +9,7 @@ interface FFLSelectOptionProps {
 const FFLSelectOption: React.FC<FFLSelectOptionProps> = ({ ffl, handleSelectFFL }) => {
     return (
         <div onClick={() => handleSelectFFL(ffl)}>
-            <h2>{ ffl.address.company }</h2>
+            <h2>{ ffl.address.firstName }</h2>
             <h4>{ ffl.address.address1}</h4>
             <h4>{ ffl.address.city + ", " + ffl.address.stateOrProvince + ", " + ffl.address.postalCode }</h4>
         </div>
--- packages/core/src/app/fflShipping/FFLSelectorSection.tsx
+++ packages/core/src/app/fflShipping/FFLSelectorSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FFLSelector from './FFLSelector';
 import { FFL } from './CustomShippingStep';
 
@@ -10,18 +10,72 @@ const FFLSelectorSection: React.FC<{
   setPickupAtSS: React.Dispatch<React.SetStateAction<boolean>>;
   shootStraightIds: number[];
   pickupAtSS: boolean;
-}> = ({ show, fflLocations, selectedFFL, setSelectedFFL, setPickupAtSS, shootStraightIds, pickupAtSS }) => {
+  shootStraightLocations: FFL[];
+  getFfls: (params: { searchTerms: string; state: string }) => Promise<void>;
+}> = ({ show, fflLocations, selectedFFL, setSelectedFFL, setPickupAtSS, getFfls, shootStraightLocations, shootStraightIds, pickupAtSS }) => {
+  const [input, setInput] = useState('');
+  const [selectedState, setSelectedState] = useState('');
+
+  const stateOptions = [
+    '', 'AL', 'AZ', 'AR', 'DE', 'FL', 'GA', 'ID', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME',
+    'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NM', 'NC', 'ND', 'OH', 'OK',
+    'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WV', 'WI', 'WY'
+  ];
+
+
   const handleSelectFFL = (ffl: FFL) => {
-    if (shootStraightIds.includes(ffl?.id)) setPickupAtSS(true);
+
+    if (shootStraightIds.includes(Number(ffl?.id))) {
+      setPickupAtSS(true);
+    }
+
+
     setSelectedFFL(ffl || null);
   };
+
+  const handleGetFfls = () => {
+    getFfls({ searchTerms: input, state: selectedState });
+  }
+
   return show ? (
-    <FFLSelector
-      ffls={fflLocations}
-      handleSelectFFL={handleSelectFFL}
-      selectedFFL={selectedFFL}
-      pickupAtSS={pickupAtSS}
-    />
+    <>
+      {!pickupAtSS && (
+        <div className="ffl-search-container">
+          <input
+            className="ffl-search-input"
+            type="text"
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            placeholder="Enter FFL name, city, or zip"
+          />
+          <select
+            className="ffl-search-input"
+            value={selectedState}
+            onChange={e => setSelectedState(e.target.value)}
+            style={{ minWidth: 50 }}
+          >
+            <option value="">State</option>
+            {stateOptions.slice(1).map((abbr) => (
+              <option key={abbr} value={abbr}>{abbr}</option>
+            ))}
+          </select>
+          <button
+            className="ffl-search-button"
+            type="button"
+            onClick={handleGetFfls}
+          >
+            Search
+          </button>
+        </div>
+      )}
+      <FFLSelector
+        ffls={fflLocations}
+        handleSelectFFL={handleSelectFFL}
+        selectedFFL={selectedFFL}
+        pickupAtSS={pickupAtSS}
+        shootStraightLocations={shootStraightLocations}
+      />
+    </>
   ) : null;
 };
 
